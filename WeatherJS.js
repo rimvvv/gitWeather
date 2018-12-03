9258934d60fe547facba186c73a53f3ed36b9cbb
Roman Vorobiev <rimvvv@gmail.com>
2018-12-03 10:10:36 +0300

    New fils adding

---
+++ WeatherJS.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/* 
+        "api.openweathermap.org/data/2.5/weather",
+        {
+            "id": "569143",
+            "appid": "62e8b40750af183fb93f7c052cbd57ee"
+        },
+        function(data){
+            console.log(data);
+*/
+
+
+
